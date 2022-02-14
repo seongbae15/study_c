a8be1ee1bdfbc137b6d6121d98d15c26269b4fda
seongbae15 <pseongbae@gmail.com>
2022-02-14 23:19:49 +0900

    update ch1 quiz

---
+++ c_exSolution/c_ex1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("*******************************************\n");
+	printf("      Let's study C/C++ programming!\n");
+	printf("*******************************************\n");
+	return 0;
+}
